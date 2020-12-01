04b5e049ff76ec69d9516844bee9bb12450686c5
Ivan <ivangrachev97@gmail.com>
2020-12-01 21:14:02 +0900

    added button toggle and box-shadow

--- easybank-landing-page/index.js
+++ easybank-landing-page/index.js
@@ -3,8 +3,18 @@ let dropContent = document.querySelector('.dropdown-content');
 let nav = document.querySelector('.nav');
 let navSpacer = document.querySelector('.nav-spacer');
 
+
+
 dropBtn.onclick = function() {
+  // Showing navbar menu
   dropContent.classList.toggle('show');
+
+  // Toggle hamburger and close icons.
+  dropBtn.classList.toggle('close-btn');
+
+  // Making navbar fixed
   nav.classList.toggle('sticky');
+
+  // Add proper amount of space when navbar menu appears.
   navSpacer.classList.toggle('show-spacer');
 }
