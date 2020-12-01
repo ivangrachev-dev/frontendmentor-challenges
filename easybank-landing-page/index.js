98d6070c9347fb5f9d7e9ad4e15b0ada4f770aea
Ivan <ivangrachev97@gmail.com>
2020-12-01 16:30:43 +0900

    added sticky navbar and upgraded drop-down menu

--- easybank-landing-page/index.js
+++ easybank-landing-page/index.js
@@ -1,6 +1,10 @@
 let dropBtn = document.querySelector('.dropdown-btn');
 let dropContent = document.querySelector('.dropdown-content');
+let nav = document.querySelector('.nav');
+let navSpacer = document.querySelector('.nav-spacer');
 
 dropBtn.onclick = function() {
   dropContent.classList.toggle('show');
+  nav.classList.toggle('sticky');
+  navSpacer.classList.toggle('show-spacer');
 }
